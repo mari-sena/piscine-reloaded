dcff853f30192ee921201777ec48d52bc976d424
Mariana Sena <35273542+mari-sena@users.noreply.github.com>
2026-05-13 13:10:20 -0300

    ft_strdup - ex20

---
+++ ex20/ft_strdup.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mariafer <mariafer@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/12 18:49:02 by mariafer          #+#    #+#             */
+/*   Updated: 2026/05/12 18:49:03 by mariafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	while (*src)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (dest);
+}
+
+char	*ft_strdup(char *src)
+{
+	char	*dest;
+
+	dest = (char *)malloc(ft_strlen(src) + 1);
+	if (!dest)
+		return (NULL);
+	return (ft_strcpy(dest, src));
+}
